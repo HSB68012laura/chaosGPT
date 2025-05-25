539f71bd4da53827444268f3d0a98cfb614eb99e
HSB68012 <hsb68012@educastur.es>
2025-05-25 23:51:05 +0200

    creacion

---
+++ script.js
@@ -0,0 +1,16 @@
+body {
+    font-family: 'Courier New', monospace;
+    background-color: #0a0a23;
+    color: #00ff88;
+    margin: 0;
+    padding: 20px;
+}
+
+h1 {
+    color: #ff00ff;
+    text-shadow: 0 0 5px #00ff88;
+}
+
+.error-message {
+    color: #ff3333;
+}
